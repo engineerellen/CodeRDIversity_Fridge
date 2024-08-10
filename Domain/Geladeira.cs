26c290e5d052c6c8b77c19a6d054f464f5c7ef45
Ellen <36012338+engineerellen@users.noreply.github.com>
2024-08-10 01:57:36 -0300

    Add project files.

---
+++ Domain/Geladeira.cs
@@ -0,0 +1,70 @@
+﻿namespace Domain
+{
+    public class Geladeira
+    {
+        private Stack<Dictionary<int, string[]>> andares;
+
+        public Geladeira()
+        {
+            andares = new Stack<Dictionary<int, string[]>>();
+
+            // Floor 2 - Charcutaria, Carnes e Ovos
+            andares.Push(new Dictionary<int, string[]> {
+            { 0, new string[4] }, // Container 0
+            { 1, new string[4] }  // Container 1
+        });
+
+            // Floor 1 - Laticínios e Enlatados
+            andares.Push(new Dictionary<int, string[]> {
+            { 0, new string[4] }, // Container 0
+            { 1, new string[4] }  // Container 1
+        });
+
+            // Floor 0 - Hortifrutis
+            andares.Push(new Dictionary<int, string[]> {
+            { 0, new string[4] }, // Container 0
+            { 1, new string[4] }  // Container 1
+        });
+        }
+
+        public void InserirItem(int piso, int gaveta, int posicao, string item)
+        {
+            var arrPisos = andares.ToArray();
+
+            if (piso < 0 || piso >= arrPisos.Length || !arrPisos[piso].ContainsKey(gaveta))
+            {
+                Console.WriteLine("Andar ou gaveta invalidos.");
+                return;
+            }
+
+            if (posicao < 0 || posicao >= arrPisos[piso][gaveta].Length)
+            {
+                Console.WriteLine("posiçao invalida");
+                return;
+            }
+
+            arrPisos[piso][gaveta][posicao] = item;
+        }
+
+        public void MostraItens()
+        {
+            var arrAndares = andares.ToArray();
+            for (int piso = 0; piso < arrAndares.Length; piso++)
+            {
+                Console.WriteLine($"Andar {piso}:");
+                foreach (var gaveta in arrAndares[piso])
+                {
+                    Console.WriteLine($"  Container {gaveta.Key}:");
+                    for (int pos = 0; pos < gaveta.Value.Length; pos++)
+                    {
+                        string item = gaveta.Value[pos];
+                        if (!string.IsNullOrEmpty(item))
+                        {
+                            Console.WriteLine($"    Posição {pos}: {item}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ViewConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+
+Geladeira objGeladeira = new Geladeira();
+
+// Floor 0 - Hortifrutis
+objGeladeira.InserirItem(0, 0, 0, "Maçã");
+objGeladeira.InserirItem(0, 0, 1, "Banana");
+objGeladeira.InserirItem(0, 1, 2, "Laranja");
+
+// Floor 1 - Laticínios e Enlatados
+objGeladeira.InserirItem(1, 0, 0, "Leite");
+objGeladeira.InserirItem(1, 1, 1, "Queijo");
+objGeladeira.InserirItem(1, 1, 2, "Milho Enlatado");
+
+// Floor 2 - Charcutaria, Carnes e Ovos
+objGeladeira.InserirItem(2, 0, 0, "Presunto");
+objGeladeira.InserirItem(2, 0, 1, "Ovos");
+objGeladeira.InserirItem(2, 1, 3, "Carne");
+
+objGeladeira.MostraItens();
